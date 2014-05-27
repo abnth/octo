8863be497d539355b815eb38e525f0d3772bafee
anurag baidyanath <anurag.fadia@gmail.com>
2014-05-27 12:16:58 +0530

    used a class

--- visit.py
+++ visit.py
@@ -1,5 +1,7 @@
-def disp():
-	for i in range(0,10):
-		print "hello anurag"
+class display:
+	def disp(self):
+		for i in range(0,10):
+			print "hello anurag"
 if __name__=="__main__":
-	disp()
+	d=display()
+	d.disp()
